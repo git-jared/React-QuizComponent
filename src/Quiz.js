37189735b13ff405641724615cfb9b1b5faa77e7
Jard Krm <krjared@gmail.com>
2023-04-21 14:53:02 -0500

    create quizEnd component

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react'
 
 import QuizQuestion from './QuizQuestion'
 
+import QuizEnd from './QuizEnd'
+
 let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
     constructor(props){
         super(props)
         this.state = {quiz_position: 1}
+       
     }
     render(){
+      const isQuizEnd = (this.state.quiz_position - 1) == quizData.quiz_questions.length
+      const quizQuestion = <QuizQuestion quiz_question = {quizData.quiz_questions[this.state.quiz_position - 1]}></QuizQuestion>
       return(
-        <div> 
-          <QuizQuestion quiz_question = {quizData.quiz_questions[this.state.quiz_position - 1]}></QuizQuestion>
+        
+        <div>
+          {(isQuizEnd) ? <QuizEnd/> : quizQuestion} 
         </div>
     )}
 }
---
+++ src/QuizEnd.js
@@ -0,0 +1,17 @@
+import react from "react";
+import Componenet from react
+
+
+class QuizEnd extends Componenet {
+    render(){
+        return (
+            <div>
+                <p>Thanks for playing!</p>
+                <a href=''>Reset Quiz</a>
+            </div>
+        )
+    }
+
+}
+
+export default QuizEnd
